842a64088be6f03c0bc747fac326df5e6de175b9
Liv Flowers <oliviaflowers93@gmail.com>
2025-04-23 22:47:09 +0100

    Add title and footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,22 @@ import styles from './App.module.css';
 
 const App = () => (
   <div className={styles.wrapper}>
-    <ConfigPanel />
-    <List />
+    <div className={styles.content}>
+      <h1 className={styles.title}>Gaydient</h1>
+      <ConfigPanel />
+      <List />
+    </div>
+
+    <div className={styles.footer}>
+      <div className={styles.footerContent}>
+        <span>
+          Created by{' '}
+          <a href="https://honeycomb.engineer/@olivvybee">olivvybee</a>
+        </span>
+
+        <a href="https://github.com/olivvybee/gaydient">Source code</a>
+      </div>
+    </div>
   </div>
 );
 
